87ee802dea0cfb2ea3bfedb32d1024433942b754
Phipat Limprasutr <phipat.limprasutr@outlook.com>
2021-01-12 20:40:34 +0700

    1st refactor to async

--- src/app/counter/buttons/buttons.component.ts
+++ src/app/counter/buttons/buttons.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { decrement, increment, reset } from '../_state/counter.action';
+import { CounterState } from '../_state/counter.state';
 
 @Component({
   selector: 'app-buttons',
@@ -8,7 +9,7 @@ import { decrement, increment, reset } from '../_state/counter.action';
   styleUrls: ['./buttons.component.scss'],
 })
 export class ButtonsComponent {
-  constructor(private store: Store<{ counter: { counter: number } }>) {}
+  constructor(private store: Store<{ counter: CounterState }>) {}
 
   onIncrement() {
     this.store.dispatch(increment());
